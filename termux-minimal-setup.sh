c6744c5da696344780dad654c154e6f8a96186f8
medanisjbara <anis2834133766619@gmail.com>
2022-08-17 14:09:08 +0100

    setup sftp

--- termux-minimal-setup.sh
+++ termux-minimal-setup.sh
@@ -9,3 +9,5 @@ pkg install busybox termux-services dropbear wget
 # Starting services daemon
 souce $PREFIX/etc/profile.d/start-services.sh
 
+# sftp server
+wget -O $PREFIX/libexec/sftp-server "https://raw.githubusercontent.com/medanisjbara/Termux-minimal-setup/master/sftp-server-$(uname -m)"
